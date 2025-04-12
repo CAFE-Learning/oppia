5f80ac96c6f5f17691700016236c6cb748e95ee6
Zivanovic8 <zivanova@dickinson.edu>
2025-04-12 13:44:54 -0400

    Trying to simulate backend response in unit test

--- core/templates/pages/moderator-page/moderator-page.component.spec.ts
+++ core/templates/pages/moderator-page/moderator-page.component.spec.ts
@@ -268,16 +268,15 @@ describe('Moderator Page Component', () => {
     componentInstance.displayedFeaturedActivityReferences = [];
     componentInstance.updateDisplayedFeaturedActivityReferences(newValue1);
 
-    componentInstance[
-      'moderatorPageBackendApiService'
-    ].saveFeaturedActivityReferencesAsync = () =>
-      Promise.reject({
-        status: 400,
-        error: {
-          error:
-            'These Exploration IDs do not exist: dne_exploration. Please enter a different ID.',
-        },
-      });
+    componentInstance.ModeratorPageBackendApiService.saveFeaturedActivityReferencesAsync =
+      () =>
+        Promise.reject({
+          status: 400,
+          error: {
+            error:
+              'These Exploration IDs do not exist: dne_exploration. Please enter a different ID.',
+          },
+        });
 
     componentInstance.saveFeaturedActivityReferences();
 
